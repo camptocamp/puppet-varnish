78958d5fb0d77ff27676f9a1d22d0748d02d588c
Francois Deppierraz <francois.deppierraz@camptocamp.com>
2009-09-15 12:19:53 +0200

    Fix warning: Unrecognised escape sequence '\1'

--- manifests/init.pp
+++ manifests/init.pp
@@ -137,7 +137,7 @@ define varnish::instance($listen_address="",
 
     Debian,Ubuntu: {
       exec { "create varnish-${name} initscript":
-        command => "sed -r -e 's|(NAME=varnishd)|\1-${name}|' -e 's|(/etc/default/varnish)|\1-${name}|' /etc/init.d/varnish > /etc/init.d/varnish-${name}",
+        command => "sed -r -e 's|(NAME=varnishd)|\\1-${name}|' -e 's|(/etc/default/varnish)|\\1-${name}|' /etc/init.d/varnish > /etc/init.d/varnish-${name}",
         creates => "/etc/init.d/varnish-${name}",
         require => Package["varnish"],
       }
@@ -145,7 +145,7 @@ define varnish::instance($listen_address="",
 
     RedHat,Fedora: {
       exec { "create varnish-${name} initscript":
-        command => "sed -r -e 's|(/etc/default/varnish)|\1-${name}|' -e 's|(/var/lock/subsys/varnish)|\1-${name}|' -e 's|(/var/run/varnish.pid)|\1-${name}|' /etc/init.d/varnish > /etc/init.d/varnish-${name}",
+        command => "sed -r -e 's|(/etc/default/varnish)|\\1-${name}|' -e 's|(/var/lock/subsys/varnish)|\1-${name}|' -e 's|(/var/run/varnish.pid)|\\1-${name}|' /etc/init.d/varnish > /etc/init.d/varnish-${name}",
         creates => "/etc/init.d/varnish-${name}",
         require => Package["varnish"],
       }
