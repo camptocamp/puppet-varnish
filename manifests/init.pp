61ab5dd541732e8acb3a0ab4416a2f212ef7c3dc
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2009-12-01 14:10:44 +0100

    added dispatch-log script in default install.
    See embeded doc for more information on how to use it.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,17 @@
 
 Installs the varnish http accelerator and starts the varnishd and varnishlog
 services.
+Installs "dispatch-log" script with its init file, and starts it.
+
+Dispatch-log is a perl script which will take varnishncsa output as
+input and dispatch log according to vhosts.
+It requires a configuration file which will contain :
+- $LOGDIR: base log dir. (optional, default: /mnt/varnish)
+- $pid_file: pid file (optional, default /var/run/dispatch-log.pid)
+- a perl hashmap named "%groups" like this:
+
+$groups{'vhost'} = 'subdirectory';
+
 
 */
 class varnish {
@@ -22,6 +33,29 @@ class varnish {
     ensure  => "running",
     pattern => "bin/varnishlog",
     require => Package["varnish"],
+  }
+
+  file {"/usr/local/bin/dispatch-log":
+    ensure => present,
+    mode   => 0755,
+    group  => root,
+    owner  => root,
+    source => "puppet:///varnish/usr/local/bin/dispatch-log",
+  }
+
+  file {"/etc/init.d/dispatch-log":
+    ensure => present,
+    mode   => 0755,
+    group  => root,
+    owner  => root,
+    source => "puppet:///varnish/etc/init.d/dispatch-log",
+  }
+
+  service { "dispatch-log":
+    enable  => true,
+    ensure  => "running",
+    pattern => "bin/dispatch-log",
+    require => [ Package["varnish"], File["/etc/init.d/dispatch-log"], File["/usr/local/bin/dispatch-log"] ],
   }
 
 }
