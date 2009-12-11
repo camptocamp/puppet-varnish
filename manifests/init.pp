532d6dd5e5abae89f2989ee80fc47abdf7108fb6
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2009-12-11 11:42:06 +0100

    new class : varnish::dispatch-log
    As some people don't want to have dispatch-log installed,
    I created a new class whih will only manage this script and service.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,17 +4,6 @@
 
 Installs the varnish http accelerator and starts the varnishd and varnishlog
 services.
-Installs "dispatch-log" script with its init file, and starts it.
-
-Dispatch-log is a perl script which will take varnishncsa output as
-input and dispatch log according to vhosts.
-It requires a configuration file which will contain :
-- $LOGDIR: base log dir. (optional, default: /mnt/varnish)
-- $pid_file: pid file (optional, default /var/run/dispatch-log.pid)
-- a perl hashmap named "%groups" like this:
-
-$groups{'vhost'} = 'subdirectory';
-
 
 */
 class varnish {
@@ -35,6 +24,31 @@ class varnish {
     require => Package["varnish"],
   }
 
+}
+
+/*
+== Class varnish::dispatch-log
+
+Installs dispatch-log script and set its service
+
+Dispatch-log is a perl script which will take varnishncsa output as
+input and dispatch log according to vhosts.
+It requires a configuration file which will contain :
+- $LOGDIR: base log dir. (optional, default: /mnt/varnish)
+- $pid_file: pid file (optional, default /var/run/dispatch-log.pid)
+- a perl hashmap named "%groups" like this:
+
+$groups{'vhost'} = 'subdirectory';
+
+Warning! you cannot change $LOGDIR nor $pid_file without restarting the service
+
+Requires:
+- Class["varnish"]
+
+*/
+
+class varnish::dispatch-log {
+
   file {"/usr/local/bin/dispatch-log":
     ensure => present,
     mode   => 0755,
@@ -42,7 +56,7 @@ class varnish {
     owner  => root,
     source => "puppet:///varnish/usr/local/bin/dispatch-log",
   }
-
+  
   file {"/etc/init.d/dispatch-log":
     ensure => present,
     mode   => 0755,
@@ -50,6 +64,14 @@ class varnish {
     owner  => root,
     source => "puppet:///varnish/etc/init.d/dispatch-log",
   }
+
+  service { "dispatch-log":
+    enable  => true,
+    ensure  => "running",
+    pattern => "bin/dispatch-log",
+    require => [ Class["varnish"], File["/etc/init.d/dispatch-log"], File["/usr/local/bin/dispatch-log"] ],
+  }
+
 
 }
 
