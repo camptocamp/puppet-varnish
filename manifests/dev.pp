0e42c5ae5e8033e4ff316b1b886eb193e04fc6ff
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-09-10 15:23:03 +0200

    Lint

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -1,40 +1,43 @@
 class varnish::dev {
 
-  package { "varnish-dev":
+  package { 'varnish-dev':
     ensure => present,
-    name => $operatingsystem ? {
-      Debian => "libvarnish-dev",
-      RedHat => "varnish-libs-devel",
+    name => $::osfamily ? {
+      Debian => 'libvarnish-dev',
+      RedHat => 'varnish-libs-devel',
     },
   }
 
   # libvarnish-dev is broken on debian-squeeze. This is the workaround.
   # Details: http://mailman.verplant.org/pipermail/collectd/2010-June/003877.html
-  if ($lsbdistcodename == "squeeze" and $varnish_version == "2.1.3") {
+  if ($::lsbdistcodename == 'squeeze' and $::varnish_version == '2.1.3') {
 
     File {
       ensure => link,
-      notify => Exec["refresh ldconfig"],
-      require => Package["varnish-dev"],
+      notify => Exec['refresh ldconfig'],
+      require => Package['varnish-dev'],
     }
 
     file {
-      "/usr/lib/libvarnish.so":
-        target => "/usr/lib/libvarnish.so.1.0.0";
-      "/usr/lib/libvarnishapi.so":
-        target => "/usr/lib/libvarnishapi.so.1.0.0";
-      "/usr/lib/libvarnishcompat.so":
-        target => "/usr/lib/libvarnishcompat.so.1.0.0";
+      '/usr/lib/libvarnish.so':
+        ensure => link,
+        target => '/usr/lib/libvarnish.so.1.0.0';
+      '/usr/lib/libvarnishapi.so':
+        ensure => link,
+        target => '/usr/lib/libvarnishapi.so.1.0.0';
+      '/usr/lib/libvarnishcompat.so':
+        ensure => link,
+        target => '/usr/lib/libvarnishcompat.so.1.0.0';
     }
 
-    file { "/usr/lib/pkgconfig/varnishapi.pc":
-      ensure  => present,
-      source  => "puppet:///modules/varnish/usr/lib/pkgconfig/varnishapi.pc",
+    file { '/usr/lib/pkgconfig/varnishapi.pc':
+      ensure => present,
+      source => 'puppet:///modules/varnish/usr/lib/pkgconfig/varnishapi.pc',
     }
 
-    exec { "refresh ldconfig":
+    exec { 'refresh ldconfig':
       refreshonly => true,
-      command     => "ldconfig",
+      command     => 'ldconfig',
     }
 
   }
--- manifests/nagiosplugin.pp
+++ manifests/nagiosplugin.pp
@@ -3,39 +3,37 @@
 == Class: varnish::nagiosplugin
 
 Downloads the sources of the official nagios plugin for varnish, builds it, and
-installs it to $nagios_plugin_dir.
+installs it to $plugin_dir.
 
 Attributes:
 - *nagios_plugin_dir*: directory where you want the built plugin to be
-  installed. Defaults to "/usr/lib/nagios/plugins/contrib".
+  installed. Defaults to '/usr/lib/nagios/plugins/contrib'.
 
 Requires:
 - vcsrepo module from http://github.com/reductivelabs/puppet-vcsrepo
 
-- Package["gcc"]
-- Package["libtool"]
+- Package['gcc']
+- Package['libtool']
 or
-- Class["buildenv::c"]
+- Class['buildenv::c']
 
 */
-class varnish::nagiosplugin {
+class varnish::nagiosplugin (
+  $plugin_dir = '/usr/lib/nagios/plugins/contrib',
+) {
 
-  include varnish::dev
-
-  if ( ! $nagios_plugin_dir ) {
-    $nagios_plugin_dir = "/usr/lib/nagios/plugins/contrib"
-  }
+  include ::varnish::dev
 
   $baseurl = 'https://github.com/varnish/varnish-nagios'
 
-  case $varnish_version {
-    "2.1.1", "2.1.2": {
+  case $::varnish_version {
+    '2.1.1', '2.1.2': {
       # http://www.varnish-cache.org/trac/ticket/710
       $revision = '70e4ded1221846d4149dd743e2ac634946c313ad'
       $branch   = 'master'
-      $buildopt = ""
+      $buildopt = ''
     }
-    "2.1.3": {
+    '2.1.3': {
       $revision = '70e4ded1221846d4149dd743e2ac634946c313ad'
       $branch   = 'master'
       $buildopt = "VARNISHAPI_LIBS='-lvarnishapi -lvarnish -lvarnishcompat'"
@@ -43,48 +41,48 @@ class varnish::nagiosplugin {
     /^2\.0\./: {
       $revision = 'a64abfb7e70fd1f6b53ff64f9ddeafb7209c0b23'
       $branch   = 'master'
-      $buildopt = ""
+      $buildopt = ''
     }
     default: {
       $revision = 'HEAD'
       $branch   = 'master'
-      $buildopt = ""
+      $buildopt = ''
     }
   }
 
-  $workdir = "/usr/src/check_varnish-${varnish_version}-${revision}"
+  $workdir = "/usr/src/check_varnish-${::varnish_version}-${revision}"
 
   vcsrepo { $workdir:
     provider => 'git',
     source   => "${baseurl}/",
-    revision => "$revision",
+    revision => $revision,
   }
 
   file { "${workdir}/build-plugin.sh":
-    mode    => 0755,
+    mode    => '0755',
     content => "#!/bin/sh
 cd \$(dirname \$0) && ./autogen.sh && ./configure && make ${buildopt}
 ",
     require => [
-      Package["gcc"],
-      Package["libtool"],
-      Class["varnish::dev"],
+      Package['gcc'],
+      Package['libtool'],
+      Class['varnish::dev'],
       Vcsrepo[$workdir],
     ],
   }
 
-  exec { "build check_varnish":
+  exec { 'build check_varnish':
     command   => "${workdir}/build-plugin.sh",
     creates   => "${workdir}/check_varnish",
     require   => File["${workdir}/build-plugin.sh"],
     #logoutput => true,
   }
 
-  file { "${nagios_plugin_dir}/check_varnish":
+  file { "${plugin_dir}/check_varnish":
     ensure  => present,
-    mode    => 0755,
-    owner   => "root",
+    mode    => '0755',
+    owner   => 'root',
     source  => "file://${workdir}/check_varnish",
-    require => Exec["build check_varnish"],
+    require => Exec['build check_varnish'],
   }
 }
