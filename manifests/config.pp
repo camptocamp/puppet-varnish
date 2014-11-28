74511ca8a62559c67c0e06cbfaf10c49a88ee282
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-11-28 16:06:16 +0100

    Add Debian wheezy support

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,47 +1,41 @@
 class varnish::config {
-  case $::osfamily {
-    'Debian': {
-      include ::systemd
+  if $::osfamily == 'Debian' and ($::operatingsystemmajrelease =~ /sid/ or $::operatingsystemmajrelease >= 8) {
+    include ::systemd
 
-      file { '/etc/systemd/system/varnish.service':
-        ensure  => 'file',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        source  => '/lib/systemd/system/varnish.service',
-        replace => false,
-      }
+    file { '/etc/systemd/system/varnish.service':
+      ensure  => 'file',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      source  => '/lib/systemd/system/varnish.service',
+      replace => false,
+    }
 
-      Augeas {
-        lens   => 'Systemd.lns',
-        incl   => '/etc/systemd/system/varnish.service',
-        notify  => Exec['systemctl-daemon-reload'],
-        require => File['/etc/systemd/system/varnish.service'],
-      }
+    Augeas {
+      lens   => 'Systemd.lns',
+      incl   => '/etc/systemd/system/varnish.service',
+      notify  => Exec['systemctl-daemon-reload'],
+      require => File['/etc/systemd/system/varnish.service'],
+    }
 
-      if $::varnish::varnish_listen_address != undef or $::varnish::varnish_listen_port != undef {
-        augeas { 'address':
-          changes => "set Service/ExecStart/arguments/*[preceding-sibling::*[1][self::*[. = '-a']]] '${::varnish::varnish_listen_address}:${::varnish::varnish_listen_port}'",
-        }
+    if $::varnish::varnish_listen_address != undef or $::varnish::varnish_listen_port != undef {
+      augeas { 'address':
+        changes => "set Service/ExecStart/arguments/*[preceding-sibling::*[1][self::*[. = '-a']]] '${::varnish::varnish_listen_address}:${::varnish::varnish_listen_port}'",
       }
     }
-    'RedHat': {
-      Shellvar {
-        target   => $::varnish::params_file,
-      }
-      if $::varnish::varnish_listen_address != undef {
-        shellvar { 'VARNISH_LISTEN_ADDRESS':
-          value => $::varnish::varnish_listen_address,
-        }
-      }
-      if $::varnish::varnish_listen_port != undef {
-        shellvar { 'VARNISH_LISTEN_PORT':
-          value => $::varnish::varnish_listen_port,
-        }
+  } else {
+    Shellvar {
+      target   => $::varnish::params_file,
+    }
+    if $::varnish::varnish_listen_address != undef {
+      shellvar { 'VARNISH_LISTEN_ADDRESS':
+        value => $::varnish::varnish_listen_address,
       }
     }
-    default: {
-      fail "Unsupported Operating System family: ${::osfamily}"
+    if $::varnish::varnish_listen_port != undef {
+      shellvar { 'VARNISH_LISTEN_PORT':
+        value => $::varnish::varnish_listen_port,
+      }
     }
   }
 }
