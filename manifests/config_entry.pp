a411fbb80b109d0e3cb0b3c0c45bd35bdf3675f3
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-11-28 14:26:28 +0100

    Config_entry must notify varnish service

--- manifests/config_entry.pp
+++ manifests/config_entry.pp
@@ -3,6 +3,7 @@ define varnish::config_entry(
   $ensure = 'present',
   $key    = $name,
 ) {
+  Varnish::Config_entry[$title] ~> Service['varnish']
   shellvar { "varnish_${name}":
     ensure   => $ensure,
     target   => $::varnish::params_file,
