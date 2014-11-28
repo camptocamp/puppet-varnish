8a2898c55ecdeae506f6f2ffc0ee1b018312857f
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-11-28 14:27:19 +0100

    Update acceptance tests

--- spec/acceptance/varnish_spec.rb
+++ spec/acceptance/varnish_spec.rb
@@ -13,24 +13,12 @@ describe 'varnish' do
         class { 'varnish::repo':
           source => $source,
         }
-      EOS
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
-    end
-
-    after :all do
-      pp = <<-EOS
-        $source = $::osfamily ? {
-          'Debian' => 'distro',
-          'RedHat' => 'epel',
-        }
-        class { 'varnish::repo':
-          ensure => 'absent',
-          source => $source,
-        }
         package { 'varnish':
           ensure => 'absent',
-         }
+        } ->
+        package { 'varnish-libs':
+          ensure => 'absent',
+        }
       EOS
       apply_manifest(pp, :catch_failures => true)
       apply_manifest(pp, :catch_changes => true)
@@ -67,19 +55,12 @@ describe 'varnish' do
     before :all do
       pp = <<-EOS
         class { 'varnish::repo': }
-      EOS
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
-    end
-
-    after :all do
-      pp = <<-EOS
-        class { 'varnish::repo':
-          ensure => 'absent',
-        }
         package { 'varnish':
           ensure => 'absent',
-         }
+        } ->
+        package { 'varnish-libs':
+          ensure => 'absent',
+        }
       EOS
       apply_manifest(pp, :catch_failures => true)
       apply_manifest(pp, :catch_changes => true)
@@ -98,6 +79,34 @@ describe 'varnish' do
       end
 
       describe port(6081) do
+        it { is_expected.to be_listening }
+      end
+
+      describe port(6082) do
+        it { is_expected.to be_listening }
+        it do
+          pending 'serverspec documentation is abviously not up-to-date'
+          is_expected.to be_listening.on('127.0.0.1').with('tcp')
+        end
+      end
+    end
+
+    context 'when changing VARNISH_LISTEN_PORT' do
+      it 'should idempotently run' do
+        pp = <<-EOS
+        class { 'varnish':
+          multi_instances => false,
+        }
+        varnish::config_entry { 'VARNISH_LISTEN_PORT':
+          value => 6080,
+        }
+        EOS
+
+        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, :catch_changes => true)
+      end
+
+      describe port(6080) do
         it { is_expected.to be_listening }
       end
 
