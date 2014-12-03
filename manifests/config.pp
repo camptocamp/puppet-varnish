d91d71eb950465e3265e5cdb89c71b87f9225a07
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-12-03 16:41:39 +0100

    Use varnish_param in varnish::config

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,78 +13,35 @@ class varnish::config {
           replace => false,
         }
 
-        Augeas {
-          lens   => 'Systemd.lns',
-          incl   => '/etc/systemd/system/varnish.service',
+        Varnish_param {
+          ensure  => present,
           notify  => Exec['systemctl-daemon-reload'],
-          require => File['/etc/systemd/system/varnish.service'],
         }
-        if $::varnish::varnish_listen_address != undef or $::varnish::varnish_listen_port != undef {
-          augeas { 'address':
-            changes => "set Service/ExecStart/arguments/*[preceding-sibling::*[1][self::*[. = '-a']]] '${::varnish::varnish_listen_address}:${::varnish::varnish_listen_port}'",
-          }
+
+        varnish_param {
+          'listen_address': value => $::varnish::varnish_listen_address;
+          'listen_port':    value => $::varnish::varnish_listen_port;
         }
       } else {
         fail "${::operatingsystem}${::operatingsystemmajrelease} not yet supported"
       }
     }
     'RedHat': {
-      $params_file = $::operatingsystemmajrelease ? {
-        '6' => '/etc/sysconfig/varnish',
-        '7' => '/etc/varnish/varnish.params',
+      Varnish_param {
+        ensure  => present,
       }
-      Shellvar {
-        target   => $params_file,
-      }
-      if $::varnish::admin_listen_address != undef {
-        shellvar { 'VARNISH_ADMIN_LISTEN_ADDRESS':
-          value => $::varnish::admin_listen_address,
-        }
-      }
-      if $::varnish::admin_listen_port != undef {
-        shellvar { 'VARNISH_ADMIN_LISTEN_PORT':
-          value => $::varnish::admin_listen_port,
-        }
-      }
-      if $::varnish::group != undef {
-        shellvar { 'VARNISH_GROUP':
-          value => $::varnish::group,
-        }
-      }
-      if $::varnish::listen_address != undef {
-        shellvar { 'VARNISH_LISTEN_ADDRESS':
-          value => $::varnish::listen_address,
-        }
-      }
-      if $::varnish::listen_port != undef {
-        shellvar { 'VARNISH_LISTEN_PORT':
-          value => $::varnish::listen_port,
-        }
-      }
-      if $::varnish::secret_file != undef {
-        shellvar { 'VARNISH_SECRET_FILE':
-          value => $::varnish::secret_file,
-        }
-      }
-      if $::varnish::storage != undef {
-        shellvar { 'VARNISH_STORAGE':
-          value => $::varnish::storage,
-        }
-      }
-      if $::varnish::ttl != undef {
-        shellvar { 'VARNISH_TTL':
-          value => $::varnish::ttl,
-        }
-      }
-      if $::varnish::user != undef {
-        shellvar { 'VARNISH_USER':
-          value => $::varnish::user,
-        }
-      }
-      if $::varnish::vcl_conf != undef {
-        shellvar { 'VARNISH_VCL_CONF':
-          value => $::varnish::vcl_conf,
-        }
+
+      varnish_param {
+        'listen_address':       value => $::varnish::listen_address;
+        'listen_port':          value => $::varnish::listen_port;
+        'admin_listen_address': value => $::varnish::admin_listen_address;
+        'admin_listen_port':    value => $::varnish::admin_listen_port;
+        'group':                value => $::varnish::group;
+        'user':                 value => $::varnish::user;
+        'secret_file':          value => $::varnish::secret_file;
+        'storage':              value => $::varnish::storage;
+        'ttl':                  value => $::varnish::ttl;
+        'vcl_conf':             value => $::varnish::vcl_conf;
       }
     }
     default: {
