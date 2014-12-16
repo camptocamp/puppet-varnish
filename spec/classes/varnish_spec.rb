d5a8341b7927fa8c52a604557bc88067c8cb02ec
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-12-16 09:16:21 +0100

    Use rspec-puppet-facts

--- spec/classes/varnish_spec.rb
+++ spec/classes/varnish_spec.rb
@@ -1,11 +1,32 @@
 require 'spec_helper'
-describe 'varnish' do
-  context 'when on RedHat' do
-    let (:facts) { {
-      :operatingsystem => 'RedHat',
-      :osfamily        => 'Redhat',
-    } }
 
-    it { is_expected.to compile.with_all_deps }
+describe 'varnish' do
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      context "with multi_instances" do
+        let(:params) do
+          {
+            :multi_instances => true,
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+      end
+
+      context "with mono instance" do
+        let(:params) do
+          {
+            :multi_instances => true,
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+      end
+    end
   end
 end
