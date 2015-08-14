684a20498156064e1e77384603cc5329ad5410fd
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-08-14 09:15:42 +0200

    Allow passing arguments to varnishncsa

--- manifests/ncsa.pp
+++ manifests/ncsa.pp
@@ -1,8 +1,35 @@
-class varnish::ncsa {
+class varnish::ncsa(
+  $user     = undef,
+  $filename = undef,
+) {
   Class['varnish'] -> Class['varnish::ncsa']
 
   service { 'varnishncsa':
     ensure => running,
     enable => true,
+  }
+
+  file { '/etc/systemd/system/varnishncsa.service':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+  augeas_file { '/etc/systemd/system/varnishncsa.service':
+    base => '/lib/systemd/system/varnishncsa.service',
+  }
+  if $user {
+    augeas { 'varnishncsa user':
+      incl    => '/etc/systemd/system/varnishncsa.service',
+      lens    => 'Systemd.lns',
+      changes => "Set Service/User ${user}",
+    }
+  }
+  if $filename {
+    augeas { 'varnishncsa filename':
+      incl    => '/etc/systemd/system/varnishncsa.service',
+      lens    => 'Systemd.lns',
+      changes => "Set Service/ExecStart/arguments/*[preceding-sibling::*[1][.='-w']] ${filename}",
+    }
   }
 }
