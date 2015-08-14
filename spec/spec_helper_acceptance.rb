b48e9f132dc0f5ec689c71106209c61ffb8c0b64
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-08-14 09:19:19 +0200

    Update spec_helper_acceptance.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,28 +2,28 @@ require 'beaker-rspec'
 
 hosts.each do |host|
   # Install puppet
-  if host['platform'] =~ /debian-8/
-    install_package host, 'puppet'
+  if ENV['PUPPET_AIO']
+    install_puppet_agent_on host, {}
   else
-    install_puppet()
-  end
-  # Install ruby-augeas
-  case fact('osfamily')
-  when 'Debian'
-    install_package host, 'libaugeas-ruby'
-  when 'RedHat'
-    install_package host, 'ruby-devel'
-    install_package host, 'augeas-devel'
-    on host, 'gem install ruby-augeas --no-ri --no-rdoc'
-  else
-    puts 'Sorry, this osfamily is not supported.'
-    exit
+    install_puppet_on host
+    # Install ruby-augeas
+    case fact('osfamily')
+    when 'Debian'
+      install_package host, 'libaugeas-ruby'
+    when 'RedHat'
+      install_package host, 'ruby-devel'
+      install_package host, 'augeas-devel'
+      on host, 'gem install ruby-augeas --no-ri --no-rdoc'
+    else
+      puts 'Sorry, this osfamily is not supported.'
+      exit
+    end
   end
 end
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  module_name = module_root.split('-').last
 
   # Readable test descriptions
   c.formatter = :documentation
@@ -31,7 +31,7 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'varnish')
+    puppet_module_install(:source => module_root, :module_name => module_name)
     hosts.each do |host|
       on host, puppet('module','install','camptocamp-systemd'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module','install','herculesteam-augeasproviders_core'), { :acceptable_exit_codes => [0,1] }
