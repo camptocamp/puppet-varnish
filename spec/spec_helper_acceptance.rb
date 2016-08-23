cb953a5427afb04877f0706789ff4fe618278204
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-23 16:54:36 +0200

    Install Puppet AIO only

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,25 +1,6 @@
 require 'beaker-rspec'
 
-hosts.each do |host|
-  # Install puppet
-  if ENV['PUPPET_AIO']
-    install_puppet_agent_on host, {}
-  else
-    install_puppet_on host
-    # Install ruby-augeas
-    case fact('osfamily')
-    when 'Debian'
-      install_package host, 'libaugeas-ruby'
-    when 'RedHat'
-      install_package host, 'ruby-devel'
-      install_package host, 'augeas-devel'
-      on host, 'gem install ruby-augeas --no-ri --no-rdoc'
-    else
-      puts 'Sorry, this osfamily is not supported.'
-      exit
-    end
-  end
-end
+install_puppet_agent_on hosts, {}
 
 RSpec.configure do |c|
   module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
